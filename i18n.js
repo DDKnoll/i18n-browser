424f4d94d45982a571bd3e562a765d46f7b2afcd
Dugan Knoll <ddknoll@gmail.com>
2015-12-23 15:05:02 -0800

    no more creating a new object.  Just pass the locale data and call __(translate) on it. ;

--- i18n.js
+++ i18n.js
@@ -1,4 +1,4 @@
-/**
+/*
  * @author  Dugan Knoll<duganknoll@gmail.com>
  * @author  Then by John Resig <jeresig@gmail.com>
  * @author  Originally by Marcus Spiegel <marcus.spiegel@gmail.com>
@@ -11,129 +11,84 @@
  *
  * All we need is templating and the ability to pass in defaults
  * for locales from a data store.  It doesn't handle any loading.
- * It just needs locales to be passed in 
- * 
+ * It just needs locales to be passed in.
  * @version 0.0.1
  */
 
 // dependencies
 var vsprintf = require("sprintf").vsprintf;
 
+var i18n = module.exports = function (locale, opt) {
+    'use strict';
+    var self = {};
+    self.locale = locale;
 
-function dotNotation (obj, is, value) {
-    if (obj.hasOwnProperty(is)) {
-        return obj[is];
-    }
-
-    if (typeof is === 'string') {
-        return dotNotation(obj, is.split('.'), value);
-    } else if (is.length === 1 && value !== undefined) {
-        return obj[is[0]] = value;
-    } else if (is.length === 0) {
-        return obj;
-    } else {
-        if (obj.hasOwnProperty(is[0])) {
-            return dotNotation(obj[is[0]], is.slice(1), value);
-        } else {
-            return obj[is.join('.')] = is.join('.');
-        }
-    }
-}
-
-var i18n = module.exports = function (locales, opt) {
-    this.locales = locales
-
-    //  Short Circuit if no locales
-    if (!locales) {
+    //  Short Circuit if no locale
+    if (!locale) {
         return false;
-    } else if (Array.isArray(locales)){
-        console.log('passing in an array of locales is currently unsupported');
-        return false;
-    } else {
-    // TODO: Do we need to do anything to the data?
-
     }
-    var self = this;
-
-    // Put into dev or production mode
-    this.devMode = process.env.NODE_ENV !== "production";
 
     // Copy over options
     for (var prop in opt) {
-        this[prop] = opt[prop];
+        self[prop] = opt[prop];
     }
 
     // you may register helpers in global scope, up to you
-    if (typeof this.register === "object") {
+    if (typeof self.register === "object") {
         i18n.resMethods.forEach(function(method) {
             self.register[method] = self[method].bind(self);
         });
     }
 
-    // Set the locale to the default locale
-    this.setLocale(this.defaultLocale);
 
-    // Check the defaultLocale
-    if (!this.locales[this.defaultLocale]) {
-        console.error("Not a valid default locale.");
+    if (!self.locale) {
+        self.defaultLocale = opt.locales[0];
+        self.setLocale(self.preferredLocale());
     }
 
-    if (!this.locale) {
-        this.defaultLocale = opt.locales[0];
-        this.setLocale(this.preferredLocale());
-    }
-};
+    // converts string "Test.test" to its equivalent javascript Test.test
+    var dotNotation = function (is, value) {
+        if (self.locale.hasOwnProperty(is)) {
+            return self.locale[is];
+        }
 
-i18n.registerMethods = function (helpers, req) {
-    i18n.resMethods.forEach(function (method) {
-        if (req) {
-            helpers[method] = req.i18n[method].bind(req.i18n);
+        if (typeof is === 'string') {
+            return dotNotation(self.locale, is.split('.'), value);
+        } else if (is.length === 1 && value !== undefined) {
+            return self.locale[is[0]] = value;
+        } else if (is.length === 0) {
+            return self.locale;
         } else {
-            helpers[method] = function (req) {
-                return req.i18n[method].bind(req.i18n);
-            };
+            if (self.locale.hasOwnProperty(is[0])) {
+                return dotNotation(self.locale[is[0]], is.slice(1), value);
+            } else {
+                return self.locale[is.join('.')] = is.join('.');
+            }
         }
-    });
-    return helpers;
-};
+    }.bind(self);
 
-i18n.prototype = {
-    defaultLocale: "en",
-    extension: ".js",
-    url: "locales",
-    cookieName: "lang",
-    sessionVarName: "locale",
-    indent: "\t",
+    // read locale file, translate a msg.
+    // private function. Use i18n(locale).__("translate") or i18n(locale).__n("test");
+    var translate = function (singular, plural) {
+        return dotNotation(singular, plural ? { one: singular, other: plural } : undefined);
+    }.bind(self);
 
-    parse: JSON.parse,
-
-    dump: function (data, indent) {
-        return JSON.stringify(data, null, indent);
-    },
-
-    __: function () {
-        var msg = this.translate(this.locale, arguments[0]);
-
-        if (arguments.length > 1) {
-            msg = vsprintf(msg, Array.prototype.slice.call(arguments, 1));
-        }
-
-        return msg;
-    },
-
-    __n: function (pathOrSingular, countOrPlural, additionalOrCount) {
+    /*****
+     * PUBLIC METHODS
+     */
+    self.__n = function (pathOrSingular, countOrPlural, additionalOrCount) {
         var msg, count;
         if (typeof countOrPlural === 'number') {
             var path = pathOrSingular;
             count = countOrPlural;
-            msg = this.translate(this.locale, path);
+            msg = translate(path);
 
             msg = vsprintf(parseInt(count, 10) > 1 ? msg.other : msg.one, Array.prototype.slice.call(arguments, 1));
         } else {
             var singular = pathOrSingular;
             var plural = countOrPlural;
             count = additionalOrCount;
-            msg = this.translate(this.locale, singular, plural);
+            msg = translate(singular, plural);
 
             msg = vsprintf(parseInt(count, 10) > 1 ? msg.other : msg.one, [count]);
 
@@ -143,86 +98,17 @@ i18n.prototype = {
         }
 
         return msg;
-    },
+    };
 
-    setLocale: function (locale) {
+    self.__ = function () {
+        var msg = translate(arguments[0]);
 
-        if (!locale) return;
-
-        if (!this.locales[locale]) {
-            if (this.devMode) {
-                console.warn("Locale (" + locale + ") not found.");
-            }
-
-            locale = this.defaultLocale;
+        if (arguments.length > 1) {
+            msg = vsprintf(msg, Array.prototype.slice.call(arguments, 1));
         }
 
-        return (this.locale = locale);
-    },
+        return msg;
+    };
 
-    getLocale: function () {
-        return this.locale;
-    },
-
-    isPreferredLocale: function () {
-        return !this.prefLocale ||
-        this.prefLocale === this.getLocale();
-    },
-
-    preferredLocale: function (req) {
-        req = req || this.request;
-
-        if (!req || !req.headers) {
-            return;
-        }
-
-        var accept = req.headers["accept-language"] || "",
-            regExp = /(^|,\s*)([a-z0-9-]+)/gi,
-            self = this,
-            prefLocale,
-            match;
-
-        while (!prefLocale && (match = regExp.exec(accept))) {
-            var locale = match[2].toLowerCase();
-            var parts = locale.split("-");
-
-            if (self.locales[locale]) {
-                prefLocale = locale;
-            } else if (parts.length > 1 && self.locales[parts[0]]) {
-                prefLocale = parts[0];
-            }
-        }
-
-        return prefLocale || this.defaultLocale;
-    },
-
-    // read locale file, translate a msg
-    translate: function (locale, singular, plural) {
-        if (!locale || !this.locales[locale]) {
-            if (this.devMode) {
-                console.warn("WARN: Locale not found. Using the default (" +
-                this.defaultLocale + ") as current locale");
-            }
-
-            locale = this.defaultLocale;
-
-            this.initLocale(locale, {});
-        }
-
-        return dotNotation(this.locales[locale], singular, plural ? { one: singular, other: plural } : undefined);
-    },
-
-
-    initLocale: function (locale, data) {
-        if (!this.locales[locale]) {
-            this.locales[locale] = data;
-            // Only cache the files when we're not in dev mode
-            if (!this.devMode) {
-                var file = this.locateFile(locale);
-                if (!i18n.localeCache[file]) {
-                    i18n.localeCache[file] = data;
-                }
-            }
-        }
-    }
+    return self;
 };
--- test/i18n.configure.js
+++ test/i18n.configure.js
@@ -1,24 +1,15 @@
 // Run $ expresso
-var I18n = require('../i18n'),
+var I18n = require('../index'),
 	fs = require('fs'),
 	assert = require('assert');
 
-var locales = JSON.parse(fs.readFileSync('locales/locales.js', 'utf8'));
+var locales = JSON.parse(fs.readFileSync('locales/en.js', 'utf8'));
 
 module.exports = {
-	'check set/getLocale': function () {
-		var i18n = new I18n(locales);
-
-		var loc = i18n.getLocale();
-		assert.equal('en', i18n.getLocale(), 'should return default setting');
-		assert.equal('de', i18n.setLocale('de'), 'should return the new setting');
-		assert.equal('de', i18n.getLocale(), 'should return the new setting');
-	},
 
 	'check singular': function () {
-		var i18n = new I18n(locales);
+		var i18n = I18n(locales);
 
-		i18n.setLocale('en');
 		assert.equal(i18n.__('Hello'), 'Hello');
 		assert.equal(i18n.__('Hello %s, how are you today?', 'Marcus'), 'Hello Marcus, how are you today?');
 		assert.equal(i18n.__('Hello %s, how are you today? How was your %s.', 'Marcus', i18n.__('weekend')), 'Hello Marcus, how are you today? How was your weekend.');
